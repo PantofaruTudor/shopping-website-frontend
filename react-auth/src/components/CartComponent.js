2ad3a97630c81ed61200be96bb9d8bcc0197ef4a
PantofaruTudor <tudorpantofaru@gmail.com>
2025-12-24 09:50:05 +0200

    fix nr 3 for netlify

--- react-auth/src/components/CartComponent.js
+++ react-auth/src/components/CartComponent.js
@@ -37,7 +37,7 @@ const CartComponent = () =>{
                                     onClick={() => removeItemCart(item.productID)}
                                     className="remove-btn"
                                 >
-                                    <a>remove</a>
+                                    remove
                                 </button>
                             </div>
                         ))
